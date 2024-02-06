83da77e5ada6b7315dad7a007e15c19abae557bb
jeongseoyoon <jsydjsy@gmail.com>
2024-02-07 00:11:27 +0900

    test

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="box">
-      정서윤
+    <div>
+      <div className="box">
+        정서윤
+      </div>
+      <div className="box">
+        정서윤
+      </div>
     </div>
   );
 }
